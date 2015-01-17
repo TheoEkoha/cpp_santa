af45306dac124740849c7c5c8f55e3230e488c9e
mathon_j <mathon_j@epitech.eu>
2015-01-17 14:45:12 +0100

    Toy

--- headers/Toy.hh
+++ headers/Toy.hh
@@ -5,7 +5,7 @@
 ** Login   <mathon_j@mathonj>
 ** 
 ** Started on  Sat Jan 17 14:05:45 2015 Jérémy MATHON
-** Last update Sat Jan 17 14:35:33 2015 Jérémy MATHON
+** Last update Sat Jan 17 14:44:23 2015 Jérémy MATHON
 */
 
 #ifndef TOY_HH_
@@ -17,7 +17,7 @@ class	Toy : public Object
 {
   const std::string	title;
 public:
-		Toy();
+  Toy(std::string const &);
   virtual	~Toy();
   virtual void	isTaken();
 };
