d952c5244da96247a3512953d572c194de17842f
mathon_j <mathon_j@epitech.eu>
2015-01-17 14:27:36 +0100

    add toy constructor

---
+++ sources/Toy.cpp
@@ -0,0 +1,23 @@
+/*
+** Toy.cpp for Toy in /home/mathon_j/rendu/cpp_santa/sources
+** 
+** Made by Jérémy MATHON
+** Login   <mathon_j@mathonj>
+** 
+** Started on  Sat Jan 17 14:24:51 2015 Jérémy MATHON
+** Last update Sat Jan 17 14:26:20 2015 Jérémy MATHON
+*/
+
+#include	"Toy.hh"
+
+Toy::Toy()
+{
+}
+
+Toy::~Toy()
+{
+}
+
+void	Toy::isTaken()
+{
+}
