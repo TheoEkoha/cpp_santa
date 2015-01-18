518dd975d0c036b238a72ddd010f45a822dc5cff
namaneo <namaneo@epitech.eu>
2015-01-18 04:46:07 +0100

    rendu

--- headers/Elf.hh
+++ headers/Elf.hh
@@ -1,10 +1,16 @@
 #ifndef _ELF_HH_
 # define _ELF_HH_
 
+#include "Object.hh"
+#include "Table.hh"
+#include "ConveyorBelt.hh"
+
 class IElf
 {
 public:
 	virtual ~IElf() {}
+	virtual bool Take() = 0;
+	virtual bool Put() = 0;
 };
 
 class Elf : public IElf
@@ -12,12 +18,21 @@ class Elf : public IElf
 protected:
 	ITable *_table;
 	IConveyorBelt *_cb;
-	Toy *_toy;
+	Object *_obj;
 	Wrap *_wrap;
 public:
-	Elf();
+	Elf(ITable *, IConveyorBelt *);
 	~Elf();
-	
+	bool TakeTable(size_t);
+	bool PutTable(int);
+	bool TakeCB();
+	bool PutCB(int);
+};
+
+enum objType
+{
+	WRAP = 1,
+	OBJECT = 2
 };
 
 #endif
--- sources/Elf.cpp
+++ sources/Elf.cpp
@@ -1,2 +1,64 @@
 #include "Elf.hh"
 
+Elf::Elf(ITable *table, IConveyorBelt *cb)
+{
+	this->_table = table;
+	this->_cb = cb;
+	this->_obj = NULL;
+	this->_wrap = NULL;
+}
+
+Elf::~Elf()
+{
+
+}
+
+void Elf::TakeTable(size_t pos)
+{
+	if ((Object *tmp = this->_table->Take(pos)) == true)
+	{
+		if ((tmp->getTitle() == "Box" || tmp->getTitle() == "GiftPaper") 
+			&& this->_wrap == NULL) 
+			this->_wrap = tmp;
+		else if (this->_obj == NULL)
+			this->_obj = tmp;
+		else
+			this->_table->Put(tmp);
+	}
+}
+
+void Elf::PutTable(int type)
+{
+	Object *tmp = (type == WRAP ? this->_wrap : this->_obj);
+	if (this->_table->Put(tmp) == true)
+	{
+		if (type == WRAP)
+			this->_wrap = NULL;
+		else
+			this->_obj = NULL;
+	}
+}
+
+void Elf::TakeCB()
+{
+	if ((Object *tmp = this->_cb->Take(pos)) == true)
+	{
+		if ((tmp->getTitle() == "Box" || tmp->getTitle() == "GiftPaper") 
+			&& this->_wrap == NULL) 
+			this->_wrap = tmp;
+		else if (this->_obj == NULL)
+			this->_obj = tmp;
+		else
+			this->_table->Put(tmp);
+	}
+}
+
+void Elf::PutCB(int type)
+{
+	if (type == WRAP && this->_wrap != NULL 
+		&& this->_cb->Put(this->_wrap) == true)
+		this->_wrap = NULL;
+	else if ((this->_obj->getTitle() == "Box" || this->_obj->getTitle() == "GiftPaper") 
+		&& this->_obj != NULL && this->_cb->Put(this->_obj) == true)
+		this->_obj = NULL;
+}
